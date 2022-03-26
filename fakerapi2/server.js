40bba4f576aa3e092980b59a5d826dc76ef175f0
adicp <adelajda.cupi@gmail.com>
2022-03-26 09:02:34 +0100

    ok

--- fakerapi2/server.js
+++ fakerapi2/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const faker = require ("faker");
 const app = express();
-const port = 8000;
+const port = 3000;
 
 // Create 2 functions: createUser, createCompany that return an object with the properties listed above
 // User Object
@@ -24,29 +24,37 @@ const port = 8000;
 // country
 
 const createUser = () => {
-    // const newUser = {
-    //     _id: faker.commerce._id(),
-    //     firstName: faker.commerce.firstName(),
-    //     lastName: faker.commerce.lastName(),
-    //     phoneNumber: faker.commerce.phoneNumber(),
-    //     email: faker.commerce.email(),
-    //     password: faker.commerce.password()
-    // };
+    const newUser = {
+        _id: faker.datatype.number(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    };
     return newUser;
 };
 
-const Company = () => {
-    const newCompany = {
-        _id: faker.company._id(),
-        name: faker.company.name(),
-        address: faker.company.address(),
-        street: faker.company.street(),
-        city: faker.company.city(),
-        state: faker.company.state(),
-        zipCode: faker.company.zipCode(),
-        country: faker.company.country()
-    };
-    return newCompany;
-};
+// const Company = () => {
+//     const newCompany = {
+//         _id: faker.company._id(),
+//         name: faker.company.name(),
+//         address: faker.company.address(),
+//         street: faker.company.street(),
+//         city: faker.company.city(),
+//         state: faker.company.state(),
+//         zipCode: faker.company.zipCode(),
+//         country: faker.company.country()
+//     };
+//     return newCompany;
+// };
 const newAccount = createUser();
-console.log(newAccount);
+// console.log(faker.name);
+
+app.get("/api", (req, res) => {
+    res.json( {newAccount} );
+});
+
+// const server = app.listen(8000, () =>
+//   console.log(`Server is locked and loaded on port ${server.address().port}!`)
+// );
